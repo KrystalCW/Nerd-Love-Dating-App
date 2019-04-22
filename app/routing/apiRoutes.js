c2b820af23f27ef51cc278629d2d4fa44e8af29b
KrystalCW <krystal.wiederrich@gmail.com>
2019-04-21 21:27:50 -0700

    worked on styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,5 +9,21 @@ module.exports = function(app) {
 
   app.post("/api/nerds", function(req, res) {
     nerdsData.push(req.body);
+    res.json(true);
   });
 };
+
+
+// app.post("/api/tables", function(req, res) {
+//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+//     // It will do this by sending out the value "true" have a table
+//     // req.body is available since we're using the body parsing middleware
+//     if (tableData.length < 5) {
+//       tableData.push(req.body);
+//       res.json(true);
+//     }
+//     else {
+//       waitListData.push(req.body);
+//       res.json(false);
+//     }
+//   });
